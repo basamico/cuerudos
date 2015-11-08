bdc5e2c817693517e0ea18e5aaf2e31fe7ba50e8
Israel Bazán Osorio <basamico@gmail.com>
2015-11-07 23:57:12 -0600

    Generando structura del proyecto

---
+++ app/controllers/catalogos_controller.rb
@@ -0,0 +1,4 @@
+class CatalogosController < ApplicationController
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cuerudos::Application.config.secret_token = '4c89d7447cff1e41c22f72810ed75ab8827bfe7bd4ff02a61e454478aefff32d84e0fb83b482d4d4bfd4147baecd421ea36d3526db1f35d557020c08d0db85fc'
